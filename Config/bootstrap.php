569469b8b55a6985df7230e66cbffdbae778098a
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-06 13:55:58 -0300

    Adicionado array de configuração para os perfis dos usuários.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,28 +1,5 @@
 <?php
-/**
- * This file is loaded automatically by the app/webroot/index.php file after core.php
- *
- * This file should load/create any application wide configuration settings, such as
- * Caching, Logging, loading additional configuration files.
- *
- * You should also use this file to include any files that provide global functions/constants
- * that your application uses.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Config
- * @since         CakePHP(tm) v 0.10.8.2117
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
+App::uses('Usuario', 'Model');
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File', 'mask' => '0777'));
 
@@ -65,4 +42,9 @@ Cache::config('default', array('engine' => 'File', 'mask' => '0777'));
  */
 CakePlugin::loadAll(array(
 	'CakePtbr' => array('bootstrap' => true),
+));
+
+Configure::write('Usuario.perfis', array(
+	Usuario::PERFIL_ADMIN => 'Administrador',
+	Usuario::PERFIL_COMUM => 'Comum',
 ));
