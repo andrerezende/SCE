361915bea3e1d08986ac68664cc7238e6eec1baa
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-07 15:07:45 -0300

    Configurações do componente Auth.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -1,26 +1,6 @@
 <?php
-/**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 App::uses('Controller', 'Controller');
+App::uses('Usuario', 'Model');
 
 /**
  * Application Controller
@@ -32,4 +12,92 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+	public $helpers = array(
+		'Html',
+		'Form',
+		'Session',
+		'Time',
+	);
+
+/**
+ * Array containing the names of components this controller uses. Component names
+ * should not contain the "Component" portion of the classname.
+ *
+ * @access public
+ * @var array
+ * @link http://book.cakephp.org/view/961/components-helpers-and-uses
+ */
+	public $components = array(
+		'Session',
+		'Auth' => array(
+			'loginAction' => array('controller' => 'usuarios', 'action' => 'login'),
+			'loginRedirect' => array('controller' => 'perguntas', 'action' => 'index'),
+			'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home'),
+			'authorize' => array('Controller'),
+		),
+		'DebugKit.Toolbar',
+	);
+
+	public function beforeFilter() {
+		$this->setUpAuth();
+	}
+
+	public function beforeRender() {
+		$this->setUpUser();
+	}
+
+/**
+ * isAutorized method
+ *
+ * @access public
+ * @param array $usuario
+ * @return boolean
+ */
+	public function isAuthorized($usuario) {
+		if (isset($usuario['perfil']) && $usuario['perfil'] == Usuario::PERFIL_ADMIN) {
+			return true;
+		}
+		return false;
+	}
+
+/**
+ * setUpAuth method
+ *
+ * @access protected
+ * @return void
+ */
+	protected function setUpAuth() {
+		$this->Auth->authenticate = array(
+			AuthComponent::ALL => array(
+				'userModel' => 'Usuario',
+				'fields' => array(
+					'username' => 'login',
+					'password' => 'senha',
+				),
+				'scope' => array(
+					'Usuario.ativo' => true,
+				),
+			),
+			'Form',
+		);
+	}
+
+/**
+ * setUpUser method
+ *
+ * @access protected
+ * @return void
+ */
+	protected function setUpUser() {
+		$isAdmin = false;
+		$userData = $this->Auth->user();
+		if ($userData) {
+			if ($userData['perfil'] == Usuario::PERFIL_ADMIN) {
+				$isAdmin = true;
+			}
+			$this->set(compact('userData', 'isAdmin'));
+		}
+	}
+
 }
