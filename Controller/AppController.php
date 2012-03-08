efa5a74a9d4e26618c86e5b596df7028b27d5e6a
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-08 02:49:41 -0300

    Removendo o jquery do debug_kit do layout.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -57,6 +57,7 @@ class AppController extends Controller {
  */
 	public function beforeRender() {
 		$this->setUpUser();
+		$this->set('debugToolbarJavascript', array('libs' => array('/debug_kit/js/js_debug_toolbar.js')));
 	}
 
 /**
