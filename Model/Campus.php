dea4de1817aa946d8eb2a923425f4f217ecdee05
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-08 02:50:55 -0300

    Adicionado o dislpayField e a validação.

--- Model/Campus.php
+++ Model/Campus.php
@@ -7,7 +7,37 @@ App::uses('AppModel', 'Model');
  */
 class Campus extends AppModel {
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'campus';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'nome';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'nome' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
 
 /**
  * hasMany associations
