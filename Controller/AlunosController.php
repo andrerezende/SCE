a0eecd140590a1ec6be1f2c7edaa05261cef5af8
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-15 23:51:45 -0300

    Adicionado o método isAuthorized().

--- Controller/AlunosController.php
+++ Controller/AlunosController.php
@@ -8,13 +8,50 @@ App::uses('AppController', 'Controller');
 class AlunosController extends AppController {
 
 /**
+ * Paginate settings
+ *
+ * @var array
+ * @access public
+ */
+	public $paginate;
+
+/**
+ * isAutorized method
+ *
+ * @access public
+ * @param array $usuario
+ * @return boolean
+ */
+	public function isAuthorized($usuario) {
+		if (parent::isAuthorized($usuario)) {
+			return true;
+		}
+		if (in_array($this->action, array('edit', 'delete'))) {
+			$alunoId = $this->request->params['pass'][0];
+			if ($this->Aluno->mesmoCampus((int)$alunoId, $usuario['campus_id'])) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+/**
  * index method
  *
  * @return void
  */
 	public function index() {
-		$this->Aluno->recursive = 0;
-		$this->set('alunos', $this->paginate());
+		$campus_id = $this->Auth->user('campus_id');
+		if (!is_null($campus_id)) {
+			$this->paginate = array(
+				'Aluno' => array(
+					'contain' => array('Curso'),
+					'conditions' => array('Curso.campus_id' => $this->Auth->user('campus_id'))
+				)
+			);
+		}
+		$this->set('alunos', $this->paginate('Aluno'));
 	}
 
 /**
@@ -29,7 +66,6 @@ class AlunosController extends AppController {
 			throw new NotFoundException(__('Invalid aluno'));
 		}
 		$respostas = $this->Aluno->AlunoResposta->find('all', array('conditions' => array('aluno_id' => $id), 'contain' => array('Resposta' => array('Pergunta'))));
-//		$perguntas = $this->Aluno->AlunoResposta->Resposta->Pergunta->find('list');
 		$aluno = $this->Aluno->read(null, $id);
 		$this->set(compact('aluno', 'respostas', 'perguntas'));
 	}
@@ -43,15 +79,14 @@ class AlunosController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Aluno->create();
 			if ($this->Aluno->save($this->request->data)) {
-				$this->Session->setFlash(__('The aluno has been saved'));
+				$this->Session->setFlash(__('The aluno has been saved'), 'flash_success');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The aluno could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The aluno could not be saved. Please, try again.'), 'flash_failure');
 			}
 		}
-		$cursos = $this->Aluno->Curso->find('list');
-		$respostas = $this->Aluno->AlunoResposta->Resposta->find('list');
-		$this->set(compact('cursos', 'respostas'));
+		$cursos = $this->Aluno->Curso->find('list', array('conditions' => array('Curso.campus_id' => $this->Auth->user('campus_id'))));
+		$this->set(compact('cursos'));
 	}
 
 /**
@@ -67,17 +102,16 @@ class AlunosController extends AppController {
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->Aluno->save($this->request->data)) {
-				$this->Session->setFlash(__('The aluno has been saved'));
+				$this->Session->setFlash(__('The aluno has been saved'), 'flash_success');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The aluno could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The aluno could not be saved. Please, try again.'), 'flash_failure');
 			}
 		} else {
 			$this->request->data = $this->Aluno->read(null, $id);
 		}
-		$cursos = $this->Aluno->Curso->find('list');
-		$respostas = $this->Aluno->Resposta->find('list');
-		$this->set(compact('cursos', 'respostas'));
+		$cursos = $this->Aluno->Curso->find('list', array('conditions' => array('Curso.campus_id' => $this->Auth->user('campus_id'))));
+		$this->set(compact('cursos'));
 	}
 
 /**
@@ -95,10 +129,10 @@ class AlunosController extends AppController {
 			throw new NotFoundException(__('Invalid aluno'));
 		}
 		if ($this->Aluno->delete()) {
-			$this->Session->setFlash(__('Aluno deleted'));
+			$this->Session->setFlash(__('Aluno deleted'), 'flash_success');
 			$this->redirect(array('action' => 'index'));
 		}
-		$this->Session->setFlash(__('Aluno was not deleted'));
+		$this->Session->setFlash(__('Aluno was not deleted'), 'flash_failure');
 		$this->redirect(array('action' => 'index'));
 	}
 
@@ -106,13 +140,13 @@ class AlunosController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Aluno->create();
 			if ($this->Aluno->save($this->request->data)) {
-				$this->Session->setFlash(__('Dados da identificação do estudante salvos'));
+				$this->Session->setFlash(__('Dados da identificação do estudante salvos'), 'flash_success');
 				$this->redirect(array('action' => 'passo_dois', 'aluno_id' => $this->Aluno->id));
 			} else {
-				$this->Session->setFlash(__('Os dados não foram salvos, tente novamente'));
+				$this->Session->setFlash(__('Os dados não foram salvos, tente novamente'), 'flash_failure');
 			}
 		}
-		$cursos = $this->Aluno->Curso->find('list');
+		$cursos = $this->Aluno->Curso->find('list', array('conditions' => array('Curso.campus_id' => $this->Auth->user('campus_id'))));
 		$this->set(compact('cursos'));
 	}
 
@@ -120,10 +154,10 @@ class AlunosController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Aluno->id = $this->params->named['aluno_id'];
 			if ($this->Aluno->saveAssociated($this->request->data)) {
-				$this->Session->setFlash(__('Dados da identificação do estudante salvos'));
-				$this->redirect(array('action' => 'view', $this->params->named['aluno_id']));
+				$this->Session->setFlash(__('Dados da identificação do estudante salvos'), 'flash_success');
+				$this->redirect(array('action' => 'view', $this->Aluno->id));
 			} else {
-				$this->Session->setFlash(__('Os dados não foram salvos, tente novamente'));
+				$this->Session->setFlash(__('Os dados não foram salvos, tente novamente'), 'flash_failure');
 			}
 		}
 		$perguntas = $this->Aluno->AlunoResposta->Resposta->Pergunta->find('all');
