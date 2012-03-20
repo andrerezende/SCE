c2afe4af35f8c7a91f623d5f3ec2a0dd23d1b180
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-20 09:50:05 -0300

    Adicionado o beforeFilter no controller 'Pages'.

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -1,25 +1,5 @@
 <?php
 /**
- * Static content controller.
- *
- * This file will render views from views/pages/
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
-/**
  * Static content controller
  *
  * Override this controller by placing a copy in controllers directory of an application
@@ -42,6 +22,17 @@ class PagesController extends AppController {
  * @var array
  */
 	public $uses = array();
+
+/**
+ * beforeFilter callback
+ *
+ * @access public
+ * @return void
+ */
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('display');
+	}
 
 /**
  * Displays a view
