ed7aeb674ae52fb257b65ddbd9d5170e3c2a7c21
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-03-22 09:36:34 -0300

    Adicionada a validação para o campo 'estado_civil'. Problema #259.

--- Model/Aluno.php
+++ Model/Aluno.php
@@ -94,6 +94,16 @@ class Aluno extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'estado_civil' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 		'data_nascimento' => array(
 			'date' => array(
 				'rule' => array('date'),
